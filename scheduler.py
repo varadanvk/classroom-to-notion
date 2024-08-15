3e86d25e63289c75338c1754c2f4b648f2a9b3a7
varadanvk <varadanvk@gmail.com>
2024-08-15 20:58:36 +0000

    fixed scheduler

--- scheduler.py
+++ scheduler.py
@@ -2,13 +2,11 @@ import schedule
 import time
 from main import main
 
-main()
-
 def job():
     print("Running Classroom to Notion sync...")
     main()
 
-schedule.every(120).seconds.do(job)
+schedule.every(20).seconds.do(job)
 
 while True:
     schedule.run_pending()
